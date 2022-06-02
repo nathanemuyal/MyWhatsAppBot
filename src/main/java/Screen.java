ad6e8813955b7e91e119f4d0616063310d2e9dcd
nathanemuyal <muyal1109@gmail.com>
2022-06-02 11:23:48 +0300

    +image

--- src/main/java/Screen.java
+++ src/main/java/Screen.java
@@ -7,6 +7,7 @@ public class Screen extends JPanel {
 
     public Screen(int x, int y, int WHIDTH, int HIGHET,Color color) {
         this.setBounds(x, y, WHIDTH, HIGHET);
+        this.logo =new ImageIcon("src/main/image/WhatsApp logo.png");
 
 
 
@@ -14,9 +15,11 @@ public class Screen extends JPanel {
 
     protected  void paintComponent(Graphics g){
         super.paintComponent(g);
-        //main color
+        //main color + logo
         g.setColor(Color.white);
         g.fillRect(0,0,getWidth(),getHeight());
+        g.drawImage(this.logo.getImage(),0,0,100,20,null);
+
 
 
     }
